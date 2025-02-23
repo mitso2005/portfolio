24ac69d3c176cac57eafb18d0972a59409dcf2df
mitso2005 <gpetrakis343@gmail.com>
2025-02-23 12:30:50 +1100

    created react app

---
+++ my-portfolio/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} dimitripetrakis.com</p>
+        </footer>
+    )
+
+}
+
+export default Footer
