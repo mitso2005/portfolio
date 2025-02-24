da30770b3db6e3b273a2d1cf022e9eb40e1f26af
mitso2005 <gpetrakis343@gmail.com>
2025-02-24 21:20:47 +1100

    header and broken footer

--- my-portfolio/src/Footer.jsx
+++ my-portfolio/src/Footer.jsx
@@ -2,8 +2,30 @@
 function Footer(){
 
     return(
-        <footer>
-            <p>&copy; {new Date().getFullYear()} dimitripetrakis.com</p>
+        <footer className="bg-gray-900 text-white py-8 flex flex-col items-center space-y-4">
+            <ul className="flex flex-col items-center space-y-2">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">UGC</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            
+            {/* Social Media Links */}
+            <div className="flex space-x-6">
+                <a href="#" className="hover:text-gray-300 transition">
+                <i className="fab fa-twitter text-2xl"></i>
+                </a>
+                <a href="#" className="hover:text-gray-300 transition">
+                <i className="fab fa-github text-2xl"></i>
+                </a>
+                <a href="#" className="hover:text-gray-300 transition">
+                <i className="fab fa-linkedin text-2xl"></i>
+                </a>
+            </div>
+
+            {/* Copyright */}
+            <p className="text-sm text-gray-400">&copy; 2025 Your Name. All rights reserved.</p>
         </footer>
     )
 
--- my-portfolio/src/Header.jsx
+++ my-portfolio/src/Header.jsx
@@ -3,13 +3,12 @@ function Header(){
 
     return(
         <header>
-            <h1>My Website</h1>
             <nav>
-                <ul>
+                <ul className="fixed top-0 left-0 h-10 w-full bg-black text-white flex justify-center items-center space-x-10 shadow-md">
                     <li><a href="#">About</a></li>
                     <li><a href="#">Products</a></li>
                     <li><a href="#">Projects</a></li>
-                    <li><a href="#">Work</a></li>
+                    <li><a href="#">UGC</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
             </nav>
