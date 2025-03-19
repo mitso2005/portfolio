3d14f4e3210512c93493856981649a3b4504cd0e
mitso2005 <gpetrakis343@gmail.com>
2025-03-19 12:50:31 +1100

    added components

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import About from './About.jsx'
 
 function App() {
   return(
     <>
       <Header/>
+      <About/>
       <Footer/>
     </>
   );
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header(){
     return(
         <header>
             <nav>
+                {/* Navigation Links */}
                 <ul className="fixed top-0 left-0 h-10 w-full bg-black text-white flex justify-center items-center space-x-10 shadow-md">
                     <li><a href="#">About</a></li>
                     <li><a href="#">Products</a></li>
