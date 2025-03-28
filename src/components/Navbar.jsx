ad6f4508951c2b20eca46bd2ff26bad66d59b101
mitso2005 <gpetrakis343@gmail.com>
2025-03-28 14:39:27 +1100

    made navbar floating and round

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,25 @@ import { Link } from "react-scroll";  // Import Link from react-scroll
 
 function Navbar() {
   return (
-    <header className="bg-black text-white fixed w-full top-0 shadow-md z-50">
-      <div className="container mx-auto flex items-center py-4 px-6">
-        <h2 className="text-lg font-bold mr-auto">Dimitrios Petrakis</h2>
-        <nav>
-          <ul className="flex justify-center space-x-6">
-            {["About", "Downloads", "Projects", "Work", "Contact"].map((item) => (
-              <li key={item}>
-                <Link
-                  to={item.toLowerCase()} // Scroll to the corresponding section
-                  smooth={true} // Enable smooth scroll
-                  duration={100} // Scroll duration in milliseconds
-                  offset={-50} // Adjust the offset to account for the fixed navbar
-                  className="hover:text-red-500 transition"
-                >
-                  {item}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+    <header className="fixed top-6 left-1/2 -translate-x-1/2 bg-black bg-opacity-80 text-white 
+                      shadow-lg rounded-full backdrop-blur-md px-6 py-3 z-50 w-fit">
+      <nav>
+        <ul className="flex justify-center space-x-6">
+          {["About", "Downloads", "Projects", "Work", "Contact"].map((item) => (
+            <li key={item}>
+              <Link
+                to={item.toLowerCase()} 
+                smooth={true} 
+                duration={50} 
+                offset={-100}
+                className="font-bold text-xl hover:text-red-500 transition cursor-pointer"
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
